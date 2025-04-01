10deed42f040ca9b8ed0ea3d3e7e84d2f636db95
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-04-01 13:29:16 -0400

    Update publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java-library")
-    id("maven-publish")
+    //id("maven-publish")
     id("signing")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 group = "io.github.lumine1909"
@@ -25,45 +28,47 @@ dependencies {
 }
 
 java {
+
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    coordinates(
+        groupId = group as String,
+        artifactId = name,
+        version = version as String
+    )
 
-            pom {
-                name.set("NBT")
-                description.set("Flexible and intuitive library for reading and writing Minecraft's NBT format, with emphasis on custom tags.")
-                url.set("https://github.com/Lumine1909/NBT_Reload")
-                packaging = "jar"
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://github.com/Lumine1909/NBT_Reload/blob/main/LICENSE.md")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("dewy")
-                        name.set("Dewy REDACTED")
-                        email.set("dewy@dewy.dev")
-                    }
-                    developer {
-                        id.set("Lumine1909")
-                        name.set("Lumine1909")
-                        email.set("133463833+Lumine1909@users.noreply.github.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Lumine1909/NBT_Reload.git")
-                    developerConnection.set("scm:git:ssh://github.com/Lumine1909/NBT_Reload.git")
-                    url.set("https://github.com/Lumine1909/NBT_Reload")
-                }
+    pom {
+        name.set("nbt")
+        description.set("Flexible and intuitive library for reading and writing Minecraft's NBT format, with emphasis on custom tags.")
+        url.set("https://github.com/Lumine1909/NBT_Reload")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/Lumine1909/NBT_Reload/blob/main/LICENSE.md")
             }
         }
+
+        developers {
+            developer {
+                id.set("dewy")
+                name.set("Dewy REDACTED")
+                email.set("dewy@dewy.dev")
+            }
+            developer {
+                id.set("Lumine1909")
+                name.set("Lumine1909")
+                email.set("133463833+Lumine1909@users.noreply.github.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/Lumine1909/NBT_Reload.git")
+            developerConnection.set("scm:git:ssh://github.com/Lumine1909/NBT_Reload.git")
+            url.set("https://github.com/Lumine1909/NBT_Reload")
+        }
     }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
